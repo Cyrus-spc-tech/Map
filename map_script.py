6bc5c4b737d02535c6485acf59ee8e0519ee1fe9
Tanish Gupta <tanishgupta12389@gmail.com>
2025-05-15 12:08:11 +0530

    adding marker

--- map_script.py
+++ map_script.py
@@ -5,6 +5,13 @@ import webbrowser
 
 m = folium.Map(location=[30.741482,76.768066], zoom_start=13)
 # display(m)
+# Add a marker
+folium.Marker(
+    location=[30.741482, 76.768066],
+    popup="Hello World",
+    icon=folium.Icon(color="green"),
+).add_to(m)
+
 m.save("map.html")
 
 webbrowser.open("map.html")
